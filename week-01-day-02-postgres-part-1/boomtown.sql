ff2f768bef9bfb90c45095895b26ca41a1d9ba79
Freddie Groom <fredgroom@gmail.com>
2018-07-10 15:51:09 +0100

    second day commit 3

---
+++ week-01-day-02-postgres-part-1/boomtown.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE boomtown;
+
+CREATE USER boomtown WITH PASSWORD 'boomtown';
+
+-- then exited, and re-ran psql:
+-- psql -U boomtown -d boomtown
+
+CREATE TABLE "public"."tags" (
+    "id" serial PRIMARY KEY,
+    "title" text NOT NULL
+);
+
+CREATE TABLE "public"."users" (
+    "id" serial PRIMARY KEY,
+    "email" text NOT NULL,
+    "fullname" text NOT NULL,
+    "bio" text
+);
+
+CREATE TABLE "public"."items" (
+    "id" serial primary key,
+    "title" text not null,
+    "imageurl" text DEFAULT null,
+    "description" text not null,
+    "ownerid" integer REFERENCES users(id),
+    "borrowerid" integer REFERENCES users(id)
+);
+
+INSERT INTO tags (title)
+VALUES ('Household Items'), ('Tools'), ('Electronics'), ('Physical Media'), ('Sporting Goods'), ('Musical Instruments'), ('Recreational Equipment');
+
+INSERT INTO items (title, imageurl, description)
+Values ('Baseball', '#', 'Lightly-used baseball for you and your friend to enjoy at your next ball tournament.'), ('Purple Racing Bike', '#', 'Get exercise while having fun in the sun on this super-cool bike.'), ('Flute', '#', 'Like-new flute to lend. Cmon, you know you want to play it...');
