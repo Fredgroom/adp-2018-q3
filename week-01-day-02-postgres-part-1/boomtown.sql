0916ebdfdb4c810afe95323fc1f2710ec0b2ebd1
Freddie Groom <fredgroom@gmail.com>
2018-07-11 10:14:54 +0100

    beginning second day

--- week-01-day-02-postgres-part-1/boomtown.sql
+++ week-01-day-02-postgres-part-1/boomtown.sql
@@ -4,6 +4,7 @@ CREATE USER boomtown WITH PASSWORD 'boomtown';
 
 -- then exited, and re-ran psql:
 -- psql -U boomtown -d boomtown
+-- DROP TABLE "public"."items";
 
 CREATE TABLE "public"."tags" (
     "id" serial PRIMARY KEY,
@@ -30,4 +31,24 @@ INSERT INTO tags (title)
 VALUES ('Household Items'), ('Tools'), ('Electronics'), ('Physical Media'), ('Sporting Goods'), ('Musical Instruments'), ('Recreational Equipment');
 
 INSERT INTO items (title, imageurl, description)
-Values ('Baseball', '#', 'Lightly-used baseball for you and your friend to enjoy at your next ball tournament.'), ('Purple Racing Bike', '#', 'Get exercise while having fun in the sun on this super-cool bike.'), ('Flute', '#', 'Like-new flute to lend. Cmon, you know you want to play it...');
+Values (
+    'Baseball', 
+    '#', 
+    'Lightly-used baseball for you and your friend to enjoy at your next ball tournament.'
+), 
+(
+    'Purple Racing Bike',
+    '#',
+    'Get exercise while having fun in the sun on this super-cool bike.'
+), 
+(
+    'Flute',
+    '#', 
+    'Like-new flute to lend. Cmon, you know you want to play it...'
+);
+
+Insert into itemtags (itemid, tagid)
+values (1, 1), 
+(2, 1), (2, 5),
+(3, 3), (3, 4),
+(4, 1), (4, 5), (4, 7);
