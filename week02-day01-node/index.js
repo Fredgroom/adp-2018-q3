32bfcc315e286e005cb57a6250b66fe6dd209618
Freddie Groom <fredgroom@gmail.com>
2018-07-16 15:37:51 +0100

    node exercise 2 - 3

--- week02-day01-node/index.js
+++ week02-day01-node/index.js
@@ -1,19 +1,33 @@
 const net = require('net');
 const server = net.createServer();
 
+const fs = require('fs');
 server.on('connection', handleConnection);
 server.listen(9000, () => {
-  console.log('server listening to %j', server.address());
+    console.log('server listening to %j', server.address());
 });
 
 function handleConnection(conn) {
     const remoteAddressAndPort = `${conn.remoteAddress}:${conn.remotePort}`;
     console.log(`new client connection from ${remoteAddressAndPort}`);
+
     conn.on('data', (buffer) => {
-        console.log(`Received from ${remoteAddressAndPort}:`, buffer.toString())
+        const textFileName = 'sample.txt';
+        const data = buffer.toString();
+
+        console.log(`Received from ${remoteAddressAndPort}:`, data)
+        conn.write(`Got "${data.trim()}". Let me uppercase: "${data.trim().toUpperCase()
+            }"`)
+
+        fs.readFile(textFileName, 'utf8', (err, contents) => {
+            if (err) {
+                throw err;
+            };
+            conn.write(`\n${contents}\n`)
+        });
     });
     conn.on('error', (error) => {
-        console.log(`And Error occorred:`, error.message())
+        console.log(`And Error occurred:`, error.message())
     });
     conn.on('close', () => {
         console.log(`Closed: ${remoteAddressAndPort}`);
