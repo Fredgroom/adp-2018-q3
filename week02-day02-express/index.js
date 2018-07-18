b2da4188bcad2e94403ebeb883c7d0337679e5ca
Freddie Groom <fredgroom@gmail.com>
2018-07-18 10:51:25 +0100

    week02-day02-express Exercise 5

--- week02-day02-express/index.js
+++ week02-day02-express/index.js
@@ -1,55 +1,54 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3300;
+const PORT = process.env.PORT || 3300;
 const quotes = [
-  {
-    name: 'Fred Brooks',
-    text: 'Nine people can’t make a baby in a month.',
-  },
-  {
-    name: 'Paul Ford',
-    text: 'A computer is a clock with benefits.',
-  },
-  {
-    name: 'Linus Torvalds',
-    text: 'Talk is cheap. Show me the code.',
-  },
-];// app.get('/', function(request, response) {
-//   response.send('Hello, world!');
-// });
+    {
+        name: 'Fred Brooks',
+        text: 'Nine people can’t make a baby in a month.',
+    },
+    {
+        name: 'Paul Ford',
+        text: 'A computer is a clock with benefits.',
+    },
+    {
+        name: 'Linus Torvalds',
+        text: 'Talk is cheap. Show me the code.',
+    },
+];
 
 function diyLogger(request, response, next) {
-  const startDate = new Date();
-  const startTime = startDate.getTime();
-  const { url, method } = request;
-  const { statusCode } = response;
+    const startDate = new Date();
+    const startTime = startDate.getTime();
+    const { url, method } = request;
+    const { statusCode } = response;
 
-  response.on('finish', () => {
-      const finishDate = new Date();
-      const finishTime = finishDate.getTime();
-      const timeDelta = finishTime - startTime;
+    response.on('finish', () => {
+        const finishDate = new Date();
+        const finishTime = finishDate.getTime();
+        const timeDelta = finishTime - startTime;
 
-      console.log(`${method} ${url} ${statusCode} ${timeDelta}ms`);
-  });
-  next();
+        console.log(`${method} ${url} ${statusCode} ${timeDelta}ms`);
+    });
+    next();
 }
 
 app.use(diyLogger);
 
-
-app.use((request, response, next) => {
-  console.log(`RECEIVED ${request.method} ${request.path}`)
-  next();
-});
 app.use(express.static('public'));
 
-app.get('/', function(request, response) {
-  response.send('hello world');
+app.get('/quotes', (request, response) => {
+    response.send(quotes);
 });
-app.get('/quotes', function(request, response) {
-  response.send(quotes);
-});
+app.get('/quotes/:name', (request, response) => {
+    const { name } = request.params;
+    const matchedQuote = quotes.find((quote) => name === quote.name);
 
-app.listen(port, ()=> {
-  console.log(`Server running @ http://localhost:${port}`);
+    if (!matchedQuote) {
+        response.status(404).json('That person isn\'t quote-worthy.');
+    } else {
+        response.json(matchedQuote.text);
+    }
 });
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
