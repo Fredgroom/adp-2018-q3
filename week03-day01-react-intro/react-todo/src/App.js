3b144cf60003ebcdf89565b88c7193e4c4e53072
Freddie Groom <fredgroom@gmail.com>
2018-07-23 17:19:52 +0100

    week03-day01-react-intro/react-todo Exercise 6: arrays to objects

--- week03-day01-react-intro/react-todo/src/App.js
+++ week03-day01-react-intro/react-todo/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const ToDo = ({ordinal, todo}) => {
-  return <li>{ordinal}. {todo}</li>;
+const ToDo = ({ number, todo }) => {
+  return <li>{number}. {todo}</li>;
 }
 
 class App extends Component {
   render() {
 
-    const todos = ["React", "Redux", "SQL"];
+    const todos = [
+      { id: 0, title: 'Learn React', complete: false },
+      { id: 1, title: 'Learn Redux', complete: false },
+      { id: 2, title: 'Learn SQL', complete: false }
+    ];
+
     return <div>
-      <h1>Todos</h1>
+      <h1>So Much To Do</h1>
       <div className="todo-list">
-      <ul>
-        {todos.map((todo, index) =>
-          <ToDo key={index} todo={todo} ordinal={index+1}></ToDo>)}
-      </ul>
+        <ul>
+          {todos.map((todo, index) =>
+            <ToDo key={todo.id} todo={todo.title} number={index + 1}></ToDo>)}
+        </ul>
       </div>
     </div>;
 
