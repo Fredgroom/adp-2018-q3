f7cfbbe622ef0e87e348b97b7d2450d4912acdb9
Freddie Groom <fredgroom@gmail.com>
2018-07-30 14:32:40 +0100

    week04-day01-redux getState from counter

--- week04-day01-redux/src/main.js
+++ week04-day01-redux/src/main.js
@@ -5,10 +5,10 @@ import store from './redux/store'
 //1. action for increment: INCREMENT_COUNTER
 //2. action for decrement: DECREMENT_COUNTER
 
-console.log(store.getState())
+console.log(store.getState().counter)
 
 store.dispatch({ type: 'INCREMENT_COUNTER' })
-console.log(store.getState());
+console.log(store.getState().counter);
 
 store.dispatch({ type: 'DECREMENT_COUNTER'})
-console.log(store.getState());
+console.log(store.getState().counter);
