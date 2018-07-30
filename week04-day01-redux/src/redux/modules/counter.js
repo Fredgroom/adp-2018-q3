067a44e08ee96fc402c06474747e69338424bd8a
Freddie Groom <fredgroom@gmail.com>
2018-07-30 14:21:39 +0100

    week04-day01-redux pre-redux reducer modules

---
+++ week04-day01-redux/src/redux/modules/counter.js
@@ -0,0 +1,19 @@
+//all teh stuff to do with counting
+
+export default (state = {count: 0} , action) => {
+
+    switch (action.type) {
+        case 'INCREMENT_COUNTER': {
+            return Object.assign({}, state, {count: state.count + 1})
+            //Increment the thing
+        }
+        case 'DECREMENT_COUNTER': {
+            return Object.assign({}, state, {count: state.count - 1})
+            //Decrement the thing
+        }
+        default: {
+            return state
+        }
+    }
+}
+
--- week04-day01-redux/src/redux/reducer.js
+++ week04-day01-redux/src/redux/reducer.js
@@ -1,17 +1,5 @@
-export default (state = {count: 0} , action) => {
 
-    switch (action.type) {
-        case 'INCREMENT_COUNTER': {
-            return Object.assign({}, state, {count: state.count + 1})
-            //Increment the thing
-        }
-        case 'DECREMENT_COUNTER': {
-            return Object.assign({}, state, {count: state.count - 1})
-            //Decrement the thing
-        }
-        default: {
-            return state
-        }
-    }
-}
+import counterReducer from './modules/counter'
 
+
+export default counterReducer
