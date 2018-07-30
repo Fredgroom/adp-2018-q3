583527a1441bcbfcfc4d4823c953d4e39477355a
Freddie Groom <fredgroom@gmail.com>
2018-07-30 14:48:19 +0100

    week04-day01-redux making action type variables for errors

--- week04-day01-redux/src/redux/modules/counter.js
+++ week04-day01-redux/src/redux/modules/counter.js
@@ -1,21 +1,24 @@
 //all teh stuff to do with counting
+const INCREMENT_COUNTER = "INCREMENT_COUNTER"
+const DECREMENT_COUNTER = "DECREMENT_COUNTER"
+
 
 export const incrementCounter = () => {
-    return { type: 'INCREMENT_COUNTER'}
+    return { type: INCREMENT_COUNTER}
 }
 
 export const decrementCounter = () => {
-    return { type: 'DECREMENT_COUNTER'}
+    return { type: DECREMENT_COUNTER}
 }
 
 export default (state = {count: 0} , action) => {
 
     switch (action.type) {
-        case 'INCREMENT_COUNTER': {
+        case INCREMENT_COUNTER: {
             return Object.assign({}, state, {count: state.count + 1})
             //Increment the thing
         }
-        case 'DECREMENT_COUNTER': {
+        case DECREMENT_COUNTER: {
             return Object.assign({}, state, {count: state.count - 1})
             //Decrement the thing
         }
