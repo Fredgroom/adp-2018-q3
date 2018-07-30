c95814d29bc1bdc68546c8cec908147aac62036e
Freddie Groom <fredgroom@gmail.com>
2018-07-30 15:35:14 +0100

    week04-day01-redux implement increment buttons

--- week04-day01-redux/src/main.js
+++ week04-day01-redux/src/main.js
@@ -4,6 +4,21 @@ import {incrementCounter, decrementCounter} from './redux/modules/counter'
 // ----------
 //1. action for increment: INCREMENT_COUNTER
 //2. action for decrement: DECREMENT_COUNTER
+
+
+document.getElementById('increment').addEventListener('click', () => {
+    store.dispatch(incrementCounter())
+    document.getElementById('count').innerHTML = store.getState().counter.count; 
+});
+
+
+document.getElementById('decrement').addEventListener('click', () => {
+    store.dispatch(decrementCounter())
+    document.getElementById('count').innerHTML = store.getState().counter.count; 
+});
+
+
+
 let unsubscribe = store.subscribe(() => {
     console.log('something changed in the store');
     console.log(store.getState().counter)
