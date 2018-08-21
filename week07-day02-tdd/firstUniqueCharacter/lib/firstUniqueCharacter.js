3973a9249c4c462b317b4ab1614167a06933d486
Freddie Groom <fredgroom@gmail.com>
2018-08-21 14:04:47 +0100

    refactored directory

---
+++ week07-day02-tdd/firstUniqueCharacter/lib/firstUniqueCharacter.js
@@ -0,0 +1,39 @@
+/**
+ * @description Return first unique character
+ * @returns {String} the first uniuqe character.
+ */
+
+function firstUniqueCharacter(input) {
+
+    if (typeof input !== "string") {
+        return undefined;
+    }
+
+    if (input.length === 1) {
+        return input;
+    };
+
+    if (input.length === 2) {
+        if (input[0] !== input[1]) {
+            return input[0];
+        } else {
+            return undefined;
+        }
+    };
+    const characterCounts = {};
+    for (let i = 0; i < input.length; i++) {
+        const currentCharacter = input[i];
+
+        if (characterCounts[currentCharacter]) {
+            characterCounts[currentCharacter]++;
+        } else {
+            characterCounts[currentCharacter] = 1;
+        }
+    }
+
+    return Object.keys(characterCounts).find((key) => {
+        return characterCounts[key] === 1;
+    });
+}
+
+module.exports = firstUniqueCharacter;
