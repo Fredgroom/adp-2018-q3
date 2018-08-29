0d0f6e20d39daa52ea81805af221f074d52d815c
Freddie Groom <fredgroom@gmail.com>
2018-08-29 14:46:43 +0100

    fetched data rendering with ActivityIndicator

--- week08-day03-react-native/HelloWorld/App.js
+++ week08-day03-react-native/HelloWorld/App.js
@@ -7,31 +7,48 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Dimensions, View, FlatList, Text } from 'react-native';
+import { Platform, StyleSheet, Dimensions, View, FlatList, Text, ActivityIndicator } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'LinkedIn: Freddie-Groom\n' +
-    'Instagram: @GreddieFroom',
-  android:
-    'LinkedIn: Freddie-Groom\n' +
-    'Instagram: @GreddieFroom',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
   constructor() {
     super();
-    this.state = { data: ['Thing 1', 'Thing 2'] };
+    this.state = { 
+      data: ['Thing 1', 'Thing 2'],
+      isLoading: true, 
+    };
   }
   render() {
-    return (
+    if (this.state.isLoading) {
+     return (
+      <ActivityIndicator animating={true} size="small" color="black" />
+     );
+    } else {
+     return (
       <FlatList
-        data={this.state.data}
-        renderItem={({ item }) => <View><Text>{item}</Text></View>}
-        keyExtractor={(item, index) => index}
+       data={this.state.data}
+       renderItem={({ item }) => <View><Text>{item.name}</Text></View>}
+       keyExtractor={item => item.id.toString()}
       />
-    );
-  };
+     );
+    }
+   }
+  componentDidMount() {
+    let endpoint = 'https://api.github.com/users/octocat/repos';
+    fetch(endpoint)
+      // if fetch is successful, read our JSON out of the response
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data });
+      })
+      .catch(error => console.log(`Error fetching JSON: ${error}`));
+  }
+  componentDidUpdate() {
+    if ( this.state.data && this.state.isLoading ) {
+      this.setState({ isLoading: false });
+    }
+  }
 };
 
 var { height, width } = Dimensions.get('window');
